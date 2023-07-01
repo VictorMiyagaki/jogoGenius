2993e488d5b0fbce8a19a6305c5836392db89284
VictorMiyagaki <111473755+VictorMiyagaki@users.noreply.github.com>
2023-07-01 13:17:24 -0300

    Refinando o projeto

--- jogo.js
+++ jogo.js
@@ -1,55 +1,197 @@
-// VARIÁVEIS
+///////////////
+// VARIÁVEIS //
+///////////////
 
 const cores = ['verde', 'vermelho', 'azul', 'amarelo'];
 let listaDeCoresDoJogo = [];
 let listaDeCoresDoJogador = [];
 let jogoComecou = false;
-let nivel = 0;
+let podeTeclar = false;
+let nivel;
 
-// EVENTOS DOS BOTÕES DO TECLADO
+///////////////////////////////////////
+// EVENTOS (TECLADO, MOUSE, CELULAR) //
+///////////////////////////////////////
 
+// AÇÕES EXECUTADAS AO APERTAR AS TECLAS 'ENTER', 'A', 'S', 'Z' OU 'X'
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter' && !jogoComecou) {
         iniciarJogo();
     }
-    switch (event.key) {
-        case 'a':
-            cliqueDoBotao('verde', 'jogador');
-            break;
-        case 's':
-            cliqueDoBotao('vermelho', 'jogador');
-            break;
-        case 'z':
-            cliqueDoBotao('amarelo', 'jogador');
-            break;
-        case 'x':
-            cliqueDoBotao('azul', 'jogador');
-            break;
+    if (jogoComecou && podeTeclar && listaDeCoresDoJogador.length < listaDeCoresDoJogo.length) {
+        switch (event.key) {
+            case 'a':
+                animacaoCliqueDoBotao('verde');
+                adicionarCorClicada('verde');
+                break;
+            case 's':
+                animacaoCliqueDoBotao('vermelho');
+                adicionarCorClicada('vermelho');
+                break;
+            case 'z':
+                animacaoCliqueDoBotao('amarelo');
+                adicionarCorClicada('amarelo');
+                break;
+            case 'x':
+                animacaoCliqueDoBotao('azul');
+                adicionarCorClicada('azul');
+                break;
+        }
     }
 });
 
-// EVENTOS DO CLIQUE DO MOUSE (OU DO TOQUE NA TELA DO CELULAR)
-
+// AÇÕES EXECUTADAS AO CLICAR/TOCAR OS BOTÕES 'VERDE', 'VERMELHO', 'AMARELO' OU 'AZUL'
 document.querySelectorAll('.botao').forEach(function (botao) {
     botao.addEventListener('click', function () {
-        const corDoBotaoClicado = this.getAttribute('id');
-        cliqueDoBotao(corDoBotaoClicado, 'jogador');
+        if (jogoComecou && podeTeclar && listaDeCoresDoJogador.length < listaDeCoresDoJogo.length) {
+            const corDoBotaoClicado = this.getAttribute('id');
+            animacaoCliqueDoBotao(corDoBotaoClicado);
+            adicionarCorClicada(corDoBotaoClicado);
+        }
     })
 });
 
+// AÇÃO EXECUTADA AO CLICAR/TOCAR NA FRASE 'CLIQUE AQUI'
 document.getElementById('cliqueAqui').addEventListener('click', function () {
     iniciarJogo();
 });
 
+////////////////////
+// LÓGICA DO JOGO //
+////////////////////
+
+// PARTE 1 - INICIA O JOGO
+function iniciarJogo() {
+    podeTeclar = true;
+    jogoComecou = true;
+    nivel = 0;
+    document.getElementById('cliqueAqui').classList.add('esconder');
+    proximoNivelDoJogo();
+}
+
+// PARTE 2 - INICIA O PRÓXIMO NÍVEL DO JOGO
+function proximoNivelDoJogo() {
+    nivel++;
+    atualizarTextoNivel('Nível ' + nivel);
+    listaDeCoresDoJogo.push(corAleatoria());
+    piscarCoresDoJogo();
+}
+
+// PARTE 3 - PISCA A SEQUÊNCIA DE CORES DO JOGO
+function piscarCoresDoJogo() {
+    podeTeclar = false;
+    document.querySelector('.circuloPretoMenor').classList.add('verdeClaro');
+    setTimeout(function () {
+        for (let i = 0; i < listaDeCoresDoJogo.length; i++) {
+            setTimeout(function () {
+                animacaoCliqueDoBotao(listaDeCoresDoJogo[i]);
+                if (i === listaDeCoresDoJogo.length - 1) {
+                    setTimeout(function () {
+                        podeTeclar = true;
+                        document.querySelector('.circuloPretoMenor').classList.remove('verdeClaro');
+                    }, 500);
+                }
+            }, 500 * i);
+        };
+    }, 250);
+}
+
+// PARTE 4 - CHECA CADA RESPOSTA DO USUÁRIO
+function checarResposta() {
+    const posicaoAtual = listaDeCoresDoJogador.length - 1
+    if (listaDeCoresDoJogo[posicaoAtual] === listaDeCoresDoJogador[posicaoAtual]) {
+        if (listaDeCoresDoJogo.length === listaDeCoresDoJogador.length) {
+            setTimeout(function () {
+                listaDeCoresDoJogador = [];
+                proximoNivelDoJogo();
+            }, 1000);
+        };
+    }
+    else {
+        finalizarJogo();
+    }
+}
+
+// PARTE 5 - FINALIZA O JOGO
+function finalizarJogo() {
+    tocarSom('errado');
+    animacao(".fundoRosa", "vermelho", 300);
+    animacao('#' + 'cliqueAqui', 'esconder');
+    listaDeCoresDoJogo = [];
+    listaDeCoresDoJogador = [];
+    jogoComecou = false;
+    atualizarTextoNivel('Aperte ENTER para começar')
+}
+
+///////////////////////
+// LÓGICA DO JOGADOR // 
+///////////////////////
+
+// PARTE ÚNICA - ADICIONA UMA COR NA E CHECA A RESPOSTA
+function adicionarCorClicada(corDoBotaoClicado) {
+    listaDeCoresDoJogador.push(corDoBotaoClicado);
+    checarResposta();
+}
+
+////////////////////////
+// FUNÇÕES AUXILIARES //
+////////////////////////
+
+// ATUALIZA O TEXTO DO TÍTULO ONDE MOSTRA O NÍVEL ATUAL DO JOGO
+function atualizarTextoNivel(texto) {
+    document.getElementById('nivel').textContent = texto;
+}
+
+// RETORNA UMA COR ALEATÓRIA ENTRE 'VERMELHO', 'VERDE', 'AZUL' OU 'AMARELO
+function corAleatoria() {
+    let numeroAleatorio = Math.floor(Math.random() * 4);
+    let corAleatoria = cores[numeroAleatorio];
+    return corAleatoria;
+}
+
+// FUNÇÃO QUE FAZ O BOTÃO PISCAR E REPRODUZ UM SOM
+function animacaoCliqueDoBotao(corDoBotaoClicado) {
+    tocarSom(corDoBotaoClicado);
+    animacao('#' + corDoBotaoClicado, 'botaoAnimado', 100);
+}
+
+///////////////
+// ANIMAÇÕES //
+///////////////
+
+// ANIMAÇÃO QUE REPRODUZ UM SOM
+function tocarSom(cor) {
+    const audio = new Audio('sons/' + cor + '.mp3');
+    audio.play();
+}
+
+// ANIMAÇÃO QUE ADICIONA E REMOVE UMA CLASSE (BOTÃO PISCANDO OU FUNDO VERMELHO QUANDO O JOGADOR ERRA)
+function animacao(elemento, classe, tempo) {
+    document.querySelector(elemento).classList.add(classe);
+    setTimeout(function () {
+        document.querySelector(elemento).classList.remove(classe);
+    }, tempo);
+}
+
+////////////////////////////////
+// ANIMAÇÕES DE BOTÕES EXTRAS //
+////////////////////////////////
+
+// BOTÃO QUE FAZ O CÍRCULO GIRAR
+
 document.querySelector('.botaoGirar').addEventListener('click', function () {
     document.querySelector('.circuloPreto').classList.toggle('animacaoGiro');
     document.querySelector('.botaoGirar').classList.toggle('verde');
-})
+});
+
+// BOTÃO QUE FAZ O FUNDO VIRAR UMA BALADA
 
 document.querySelector('.botaoBalada').addEventListener('click', function () {
     document.querySelector('.fundoRosa').classList.toggle('baladinha');
     document.querySelector('.botaoBalada').classList.toggle('verde');
-})
+});
+
+// BOTÃO QUE REPRODUZ UMA MÚSICA
 
 const musica = new Audio('sons/give.mp3');
 let musicaTocando = false;
@@ -58,111 +200,15 @@ document.querySelector('.botaoMusica').addEventListener('click', function () {
     if (!musicaTocando) {
         musica.play();
         musicaTocando = true;
-        document.querySelector('.rick').style.visibility = 'visible';
-        document.querySelector('.rick2').style.visibility = 'visible';
-        document.querySelector('.fundoRosa').style.opacity = '0.9';
+        document.querySelector('.esquerdo').style.visibility = 'visible';
+        document.querySelector('.direito').style.visibility = 'visible';
         document.querySelector('.botaoMusica').textContent = "⏸️";
     } else {
         musica.pause();
         musicaTocando = false;
-        document.querySelector('.rick').style.visibility = 'hidden';
-        document.querySelector('.rick2').style.visibility = 'hidden';
+        document.querySelector('.esquerdo').style.visibility = 'hidden';
+        document.querySelector('.direito').style.visibility = 'hidden';
         document.querySelector('.botaoMusica').textContent = "▶️";
     }
-    document.querySelector('.botaoMusica').classList.toggle('verde');
+    document.querySelector('.botaoMusica').classList.toggle('botaoExtraVerde');
 })
-
-// FUNÇÃO PARA INICIAR O JOGO
-
-function iniciarJogo() {
-    jogoComecou = true;
-    nivel = 0;
-    document.getElementById('nivel').textContent = 'Nível ' + nivel;
-    document.getElementById('cliqueAqui').classList.add('esconder');
-
-    proximaCorDoJogo();
-
-}
-
-function proximaCorDoJogo() {
-    nivel++;
-    document.getElementById('nivel').textContent = 'Nível ' + nivel;
-    listaDeCoresDoJogador = [];
-    let numeroAleatorio = Math.floor(Math.random() * 4);
-    let corAleatoria = cores[numeroAleatorio];
-    listaDeCoresDoJogo.push(corAleatoria);
-
-    setTimeout(function () {
-        for (let i = 0; i < listaDeCoresDoJogo.length; i++) {
-            (function (i) {
-                setTimeout(function () {
-                    document.getElementById(listaDeCoresDoJogo[i]).classList.add('botaoAnimado');
-                    setTimeout(function () {
-                        document.getElementById(listaDeCoresDoJogo[i]).classList.remove('botaoAnimado');
-                    }, 100);
-                    const audio = new Audio('sons/' + listaDeCoresDoJogo[i] + '.mp3');
-                    audio.play();
-                }, 500 * i);
-            })(i);
-        };
-    }, 250);
-
-    // setTimeout(function () { cliqueDoBotao(corAleatoria) }, 500);
-}
-
-// FUNÇÃO DO JOGADOR
-
-function cliqueDoBotao(corDoBotaoClicado, quemEnviou) {
-    if (jogoComecou) {
-        tocarSom(corDoBotaoClicado);
-        animarBotao(corDoBotaoClicado);
-        if (quemEnviou === 'jogador') {
-            listaDeCoresDoJogador.push(corDoBotaoClicado);
-            checarResposta(listaDeCoresDoJogador.length - 1);
-        }
-    }
-}
-
-function checarResposta(posicaoAtual) {
-    if (listaDeCoresDoJogo[posicaoAtual] === listaDeCoresDoJogador[posicaoAtual]) {
-        if (listaDeCoresDoJogo.length === listaDeCoresDoJogador.length) {
-            setTimeout(function () {
-                proximaCorDoJogo();
-            }, 1000);
-        };
-    }
-    else {
-        tocarSom('errado');
-        fundoVermelho();
-        finalizarJogo();
-    }
-}
-
-function fundoVermelho() {
-    document.querySelector('.fundoRosa').classList.add("vermelho");
-    setTimeout(function () {
-        document.querySelector('.fundoRosa').classList.remove("vermelho");
-    }, 300);
-}
-
-function finalizarJogo() {
-    listaDeCoresDoJogo = [];
-    jogoComecou = false;
-    document.getElementById('nivel').textContent = 'Aperte ENTER para começar';
-    document.getElementById('cliqueAqui').classList.remove('esconder');
-}
-
-// ANIMAÇÕES
-
-function tocarSom(cor) {
-    const audio = new Audio('sons/' + cor + '.mp3');
-    audio.play();
-}
-
-function animarBotao(corDoBotaoClicado) {
-    document.getElementById(corDoBotaoClicado).classList.add('botaoAnimado');
-    setTimeout(function () {
-        document.getElementById(corDoBotaoClicado).classList.remove('botaoAnimado');
-    }, 100);
-};
-
